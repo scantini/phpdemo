c387942477e773436546cb682d368abb9fbef673
scantini <scantini@heroku.com>
2017-04-04 10:55:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
 <h3> Hello World patch 6</h3>
 <h3> Hello World from a different branch</h3>
 <h3> Hello World from another branch</h3>
-<h3> Hello World from Brussels</h3>
+<h3> Hello World from Nantes</h3>
 </php>
 
